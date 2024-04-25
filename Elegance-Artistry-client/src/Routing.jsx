e2832e99826046be2cc5d78a6a9b0f0094810aa0
rjspyk5 <rjspyk5@gmail.com>
2024-04-26 02:33:22 +0600

    setup routing path for navbar components

--- Elegance-Artistry-client/src/Routing.jsx
+++ Elegance-Artistry-client/src/Routing.jsx
@@ -1,6 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Root } from "./Layout/Root";
 import { Home } from "./Pages/Home";
+import { PrivateRoute } from "./Pages/PrivateRoute";
+import { AddArtIteam } from "./Pages/AddArtIteam";
+import { MyArtList } from "./Pages/MyArtList";
+import { AllArtAndCrafts } from "./Pages/AllArtAndCrafts";
+import { Login } from "./Pages/Login";
+import { Register } from "./Pages/Register";
 
 export const routes = createBrowserRouter([
   {
@@ -10,6 +16,28 @@ export const routes = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/arts",
+        element: <AllArtAndCrafts />,
+      },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      {
+        path: "/add",
+        element: (
+          <PrivateRoute>
+            <AddArtIteam />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/myart",
+        element: (
+          <PrivateRoute>
+            <MyArtList />
+          </PrivateRoute>
+        ),
       },
     ],
   },
