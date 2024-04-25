d0da84826d03ce3fdf6d2898f41416f224975ff5
rjspyk5 <rjspyk5@gmail.com>
2024-04-26 02:23:16 +0600

    setup react router initial setup

--- Elegance-Artistry-client/src/Layout/Root.jsx
+++ Elegance-Artistry-client/src/Layout/Root.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
+import { Navbar } from "../Components/Navbar";
+import { Footer } from "../Components/Footer";
 
 export const Root = () => {
   return (
     <div>
-      Root
+      <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
