94d796f827b9808b639302430d4d8db1bc88192e
rjspyk5 <rjspyk5@gmail.com>
2024-04-27 00:27:45 +0600

    make a slider component from keen slider for making review section

---
+++ src/Components/ArtsShowcase.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const ArtsShowcase = () => {
+  return (
+    <div>
+      <div className=" bg-main">
+        <fieldset>
+          <legend>Featured Artworks</legend>
+        </fieldset>
+
+        <hr />
+        <p>Featured Artworks</p>
+        <hr />
+      </div>
+    </div>
+  );
+};
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Carasoul } from "../Components/Carasoul";
+import { ArtsShowcase } from "../Components/ArtsShowcase";
 
 export const Home = () => {
   return (
     <div>
       <Carasoul />
+      <ArtsShowcase />
     </div>
   );
 };
