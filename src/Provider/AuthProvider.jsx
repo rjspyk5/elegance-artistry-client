02e356060cde4e3f2263be39397e34cfa4858b5b
rjspyk5 <rjspyk5@gmail.com>
2024-04-26 12:57:01 +0600

    authstate function created for observe state

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,7 +1,42 @@
-import { createContext } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase.config";
 export const AuthContext = createContext(null);
+
 export const AuthProvider = ({ children }) => {
-  const userinfo = {};
+  const [user, setuser] = useState();
+  const [loading, setloading] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (res) => {
+      setuser(res);
+      setloading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const signUp = (email, pass) => {
+    loading(true);
+    return createUserWithEmailAndPassword(auth, email, pass);
+  };
+
+  const logIn = (email, pass) => {
+    loading(true);
+    return signInWithEmailAndPassword(auth, email, pass);
+  };
+
+  const logOut = () => {
+    loading(true);
+    return signOut(auth);
+  };
+
+  const userinfo = { signUp, logIn, logOut, user, loading };
   return (
     <AuthContext.Provider value={userinfo}>{children}</AuthContext.Provider>
   );
