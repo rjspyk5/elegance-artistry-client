53ccbf41e4a348aa5714404563032222dde4feb6
rjspyk5 <rjspyk5@gmail.com>
2024-04-27 19:32:02 +0600

    load specific user data on my artlist component

--- src/Pages/MyArtList.jsx
+++ src/Pages/MyArtList.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
 
 export const MyArtList = () => {
+  const {
+    user: { email },
+  } = useContext(AuthContext);
+
+  const [userArts, setuserArts] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/myart/${email}`)
+      .then((res) => res.json())
+      .then((r) => console.log(r))
+      .catch((err) => console.log(err));
+  }, [email]);
+
   return <div>MyArtList</div>;
 };
