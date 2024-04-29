dcbecabf27c5da028ff69fd21a37527f881ecd19
rjspyk5 <rjspyk5@gmail.com>
2024-04-29 11:14:01 +0600

    catagores section repair

--- src/Components/Catagory.jsx
+++ src/Components/Catagory.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { SectionHeading } from "./SectionHeading";
 import { CatagoryCard } from "./CatagoryCard";
+import cartoon from "../assets/images/catagory/cartoon.jpg";
+import chacrol from "../assets/images/catagory/chacrol.jpg";
+import landscape from "../assets/images/catagory/landscape.jpg";
+import oil from "../assets/images/catagory/oil painting2.jpg";
+import portrait from "../assets/images/catagory/protrait.jpeg";
+import water from "../assets/images/catagory/watercolor.jpg";
 
 export const Catagory = () => {
   return (
@@ -10,12 +16,16 @@ export const Catagory = () => {
         subheading=" Explore our curated collection of contemporary and traditional artworks"
       />
       <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-        <CatagoryCard title="Landscape Painting "></CatagoryCard>
-        <CatagoryCard title="Portrait Drawing"></CatagoryCard>
-        <CatagoryCard title="Watercolour Painting"></CatagoryCard>
-        <CatagoryCard title="Oil Paintinge"></CatagoryCard>
-        <CatagoryCard title="Charcoal Sketching"></CatagoryCard>
         <CatagoryCard
+          photo={landscape}
+          title="Landscape Painting "
+        ></CatagoryCard>
+        <CatagoryCard photo={chacrol} title="Portrait Drawing"></CatagoryCard>
+        <CatagoryCard photo={oil} title="Watercolour Painting"></CatagoryCard>
+        <CatagoryCard photo={portrait} title="Oil Paintinge"></CatagoryCard>
+        <CatagoryCard photo={water} title="Charcoal Sketching"></CatagoryCard>
+        <CatagoryCard
+          photo={cartoon}
           title="
 Cartoon Drawing"
         ></CatagoryCard>
--- src/Components/CatagoryCard.jsx
+++ src/Components/CatagoryCard.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-export const CatagoryCard = ({ title }) => {
+export const CatagoryCard = ({ title, photo }) => {
   return (
-    <div className="card  hover:bg-base-100 bg-base-300 hover:cursor-pointer shadow-xl">
-      <div className="card-body">{title}</div>
+    <div
+      className={`card flex py-10 space-y-4 flex-col justify-center items-center hover:bg-base-100 bg-base-300 bg-contain hover:cursor-pointer shadow-xl`}
+    >
+      <img className="w-28 h-20 rounded-xl" src={photo} alt="" />
+      <div className="font-bold text-lg">{title}</div>
     </div>
   );
 };
