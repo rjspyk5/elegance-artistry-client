e507f9333c2a858ea97cda72d77600d1a3d654b5
rjspyk5 <rjspyk5@gmail.com>
2024-04-30 18:49:29 +0600

    all product table responsive done

--- src/Pages/AllArtAndCrafts.jsx
+++ src/Pages/AllArtAndCrafts.jsx
@@ -4,11 +4,11 @@ export const AllArtAndCrafts = () => {
   const artsData = useLoaderData();
 
   return (
-    <div className="my-10 bg-base-200 p-5 rounded-lg">
+    <div className="my-10 bg-base-200 md:p-5 mx-2 rounded-lg">
       <div className="overflow-x-auto">
         <table className="table table-xs">
           <thead>
-            <tr>
+            <tr className="*:px-1 *:md:px-3">
               <th></th>
               <th>Image</th>
               <th>Name</th>
@@ -21,24 +21,24 @@ export const AllArtAndCrafts = () => {
           <tbody>
             {artsData.map((el, idx) => {
               return (
-                <tr key={el._id}>
-                  <th>{idx + 1}</th>
-                  <td>
+                <tr className="px-1 md:px-3" key={el._id}>
+                  <th className="px-1 md:px-3">{idx + 1}</th>
+                  <td className="px-1 md:px-3">
                     <div className="avatar">
-                      <div className="mask mask-squircle w-12 h-12">
+                      <div className="mask mask-squircle w-8 h-8 md:w-12 md:h-12">
                         <img src={el.photo} alt="Arts image" />
                       </div>
                     </div>
                   </td>
-                  <td>{el.arts_name}</td>
-                  <td>{el.price}</td>
-                  <td>{el.stock}</td>
-                  <td>{el.email}</td>
-                  <td>
+                  <td className="px-1 md:px-3">{el.arts_name}</td>
+                  <td className="px-1 md:px-3">{el.price}</td>
+                  <td className="px-1 md:px-3">{el.stock}</td>
+                  <td className="px-1 md:px-3">{el.email}</td>
+                  <td className="px-1 md:px-3">
                     {" "}
                     <Link
                       to={`/art/${el._id}`}
-                      className="btn btn-ghost bg-lime-500 text-white btn-sm"
+                      className="btn px-[2px] md:px-2  bg-lime-500 text-white btn-xs md:btn-sm"
                     >
                       {" "}
                       View Details
